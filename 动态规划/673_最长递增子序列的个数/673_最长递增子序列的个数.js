673f86e30135452467669d67789ae7bd59a53f70
EasyyyLLL <einsamluo@163.com>
2024-09-03 20:54:26 +0800

    feat: 最长递增子序列个数的代码实现和解题思路

--- 动态规划/673_最长递增子序列的个数/673_最长递增子序列的个数.js
+++ 动态规划/673_最长递增子序列的个数/673_最长递增子序列的个数.js
@@ -1,18 +1,40 @@
 const findNumberOfLIS = function(nums) {
-  // dp[i] 表示以第i个元素结尾的最长递增子序列的长度
-  const dp = new Array(nums.length)
-  dp[0] = 1
-  // cnt[i] 表示以第i个元素结尾的最长递增子序列的个数
-  const cnt = new Array(nums.length)
-  cnt[0] = 1
+    const n = nums.length
+    // dp[i] 表示以第i个元素结尾的最长递增子序列的长度
+    const dp = new Array(n);
+    // cnt[i] 表示以第i个元素结尾的最长递增子序列的个数
+    const cnt = new Array(n);
+    cnt[0] = 1;
 
-  for (let i = 1; i < nums.length; i++) {
-      dp[i] = 1
-      cnt[i] = 1
-      for (let j = 0; j < i; j++) {
-          if (nums[j] < nums[i]) {
-              dp[i] = Math.max(dp[i], dp[j] + 1)
-          }
-      }
-  }
+    let maxLen = 1
+    let ret = 0
+
+    for (let i = 0; i < n; i++) {
+        dp[i] = 1
+        cnt[i] = 1
+        for (let j = 0; j < i; j++) {
+            if (nums[i] > nums[j]) {
+                if (dp[i] < dp[j] + 1) {
+                    dp[i] = dp[j] + 1
+                    cnt[i] = cnt[j]
+                } else if (dp[i] === dp[j] + 1) {
+                    cnt[i] = cnt[i] + cnt[j]
+                }
+            } 
+        }
+
+        if (dp[i] > maxLen) {
+            maxLen = dp[i]
+            ret = cnt[i]
+        } else if (dp[i] === maxLen) {
+            ret = ret + cnt[i]
+        }
+    }
+    
+    return ret
 };
+
+
+const nums = [2,2,2,2,2]
+console.log(findNumberOfLIS(nums))
+
